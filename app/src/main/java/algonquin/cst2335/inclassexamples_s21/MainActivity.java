0bbaee4872bbb839251f665b08e8c4944deeb79e
Mehdi Sibachir <m3sibachir@gmail.com>
2021-06-08 21:11:47 -0400

    Week4 Lab add functions to buttons

--- app/src/main/java/algonquin/cst2335/inclassexamples_s21/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/inclassexamples_s21/MainActivity.java
@@ -3,16 +3,21 @@ package algonquin.cst2335.inclassexamples_s21;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.FileOutputStream;
 
+public class MainActivity extends AppCompatActivity {
+EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +27,70 @@ public class MainActivity extends AppCompatActivity {
         Log.w("MainActivity", "In onCreate() - Loading Widgets" );
 
         loginBtn.setOnClickListener( clk -> {
-            EditText et = findViewById(R.id.inputEditText);
+
+            et = findViewById(R.id.inputEditText);
             Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
 
             nextPage.putExtra("SomeInfo", et.getText().toString());
-            startActivity( nextPage );
+            nextPage.putExtra("MyFloat", et.getText().toString());
+            nextPage.putExtra("IsTrue", false);
+
+            Intent call = new Intent( MediaStore.ACTION_IMAGE_CAPTURE);
+            //call.setData(Uri.parse("tel:" + "613 123 3567"));
+            startActivityForResult( nextPage, 123 );
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        String path = getFilesDir().getPath();
+        if(requestCode == 123) {
+            if (resultCode == RESULT_OK) {
+                Bitmap thumbnail = data.getParcelableExtra("data");
+                FileOutputStream fOut = null;
+
+                try {
+                    fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+                    thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                fOut.flush();
+                fOut.close();
+
+                } catch (Exception e){
+                    e.printStackTrace();
+
+                }
+                Log.w("MainActivity", "Coming back from next page");
+            }
+        }
+    }
+
+
+    //Cntl + o = overide
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.w("MainActivity", "In onStart() - now visible" );
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "in onResume() - Listen for input" );
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
--- app/src/main/java/algonquin/cst2335/inclassexamples_s21/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/inclassexamples_s21/SecondActivity.java
@@ -1,9 +1,16 @@
 package algonquin.cst2335.inclassexamples_s21;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -14,8 +21,45 @@ public class SecondActivity extends AppCompatActivity {
 
         Intent fromPrevious = getIntent();
         String text = fromPrevious.getStringExtra("SomeInfo");
+        EditText line = findViewById(R.id.editTextPhone);
+
+        String fromMain = fromPrevious.getStringExtra("MyFloat");
+        float test = fromPrevious.getFloatExtra("ABCDE", 1.0f);
+        boolean t = fromPrevious.getBooleanExtra("IsTrue", false);
+
+        Button call = findViewById(R.id.button);
+        call.setOnClickListener(v -> {
+//            Intent sendBack = new Intent();
+//            sendBack.putExtra("Number", line.getText().toString());
+//
+//            setResult(345, sendBack);
+//            finish();
+            TextView top = findViewById(R.id.textView);
 
 
 
+            Intent c = new Intent(Intent.ACTION_DIAL);
+            c.setData(Uri.parse("tel:" + "613 123 3567"));
+            startActivityForResult(c, 4735);
+        });
+
+        Button pict = findViewById(R.id.button2);
+        pict.setOnClickListener( cl -> {
+            Intent in = new Intent(Intent.ACTION_WEB_SEARCH);
+            in.putExtra(SearchManager.QUERY, "Ottawa Ontario");
+            startActivityForResult(in, 3535);
+
+        });
+        TextView top = findViewById(R.id.textView);
+
+        top.setText("Email = " + fromMain );
+
+        //line.setText(text);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
